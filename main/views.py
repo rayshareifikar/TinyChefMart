d65d7db794f2be360ca94cc7b082efd75b513890
rayshareifikar <nyananyana23@gmail.com>
2024-09-10 00:09:44 +0700

    change main.html

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'app_name' : 'Tiny Chef Mart',
+        'app' : 'Tiny Chef Mart',
         'name': 'Raysha Reifika Ryzki',
         'class': 'PBP A'
     }
